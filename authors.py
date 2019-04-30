2417d1df2b2a0b0ea65c55041d5485115b133810
Emily  Morin <35749810+emorin1994@users.noreply.github.com>
2019-04-30 17:37:25 -0400

    json hack
    Emily Dani and David

---
+++ authors.py
@@ -0,0 +1,11 @@
+import json
+with open('data.json') as jsonfile:
+    parsed = json.load(jsonfile)
+# print(type(parsed))
+for item in parsed:
+# articles = parsed['articles'][0]
+    print(item["author"])
+# for item in articles.items():
+#     print(item['author'])
+
+# # print (json.dumps(parsed, indent=2, sort_keys=True))
